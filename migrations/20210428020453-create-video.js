9c296d29dd5de879c6715c878f273295cb7f0c39
Dylan Kesterson <dlkesterson@gmail.com>
2021-04-27 21:05:51 -0500

    created Video model

---
+++ migrations/20210428020453-create-video.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Videos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      video_id: {
+        type: Sequelize.STRING
+      },
+      video_url: {
+        type: Sequelize.STRING
+      },
+      channel_id: {
+        type: Sequelize.STRING
+      },
+      thumbnail: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Videos');
+  }
+};
