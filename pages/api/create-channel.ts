0a2ce2bb35de5de8477756f46d42df3f32121450
Dylan Kesterson <dlkesterson@gmail.com>
2021-05-23 13:58:43 -0500

    file cleanup

--- pages/api/create-channel.ts
+++ pages/api/create-channel.ts
@@ -1,7 +1,5 @@
 import { NextApiHandler } from 'next'
-import { cpuUsage } from 'node:process';
 import ytpl from 'ytpl'
-// import { promises as fs } from 'fs';
 const fs = require('fs');
 const getColors = require('get-image-colors');
 const fetch = require('node-fetch');
@@ -28,9 +26,6 @@ const handler: NextApiHandler = async (req, res) => {
 
         if (videos.length > 0) {
             await saveChannelVideos(videos, transaction);
-            // videos.forEach((video, index) => {
-            //     console.log(index + ' / ' + videos.length + ' : video url : ' + video.shortUrl);
-            // });
         }
 
         await transaction.commit();
@@ -58,37 +53,12 @@ async function downloadImage(url, location, file_name) {
 }
 
 async function saveImageColors(imagePath) {
-    // const dir = `./data/${video.channel_id}`;
-    // const image = `${dir}/${video.video_id}.jpg`;
     const colors = await getColors(imagePath).then(colors => {
         return colors.map(color => color.hex()).toString();
     });
 
-    // await sequelize.models.Video.update({ colors }, { where: { video_id: video.video_id }, transaction } );
-
     return colors;
 }
-
-// async function saveChannelVideosImageColors(videos, transaction) {
-//     await videos.forEach((video) => {
-//         const dir = `./data/${video.author.channelID}`;
-//         const image = `${dir}/${video.id}.jpg`;
-//         const colors = getColors(image);
-//         console.log('typeof colors: ' + typeof colors);
-//         console.log(colors);
-
-//         sequelize.models.Video.update({ colors }, { where: { video_id: video.id }, transaction } )
-//     });
-
-// return;
-
-// await sequelize.models.Video.bulkUpdate('roles', {
-//     label: 'admin',
-//   }, {
-//     userType: 3,
-//   },
-// );
-// }
 
 async function downloadImages(videos) {
     let imagePathList = [];
@@ -113,46 +83,22 @@ async function saveChannelVideos(videos, transaction) {
     });
 
     try {
-        // let colorString;
-
-        // const imagePath = `./data/${pv.author.channelID}/${pv.id}.jpg`;
-
-        // downloadImage(pv.thumbnails[0].url, pv.author.channelID, `${pv.id}`)
-        // .then(() => {
-        //     // colorString = saveImageColors(imagePath);
-        //     colorString = getColors(imagePath).then(colors => {
-        //         return colors.map(color => color.hex()).toString();
-        //     })
-        // });
-
         await downloadImages(newVideos)
             .then(async (imagePathList) => {
-                console.log('\n\nat this point I would collect the colors from each downloaded image, and do a .map() to update newVideos "colors" prop to be filled in, before they are saved to DB\n\n');
-
-                console.log('looping over ' + newVideos.length + ' items');
                 for (let i = 0; i < newVideos.length; i++) {
-                    console.log('looking for file ' + imagePathList[i]);
                     if (fs.existsSync(imagePathList[i])) {
-                        console.log('verified that the file exists');
                         const colorString = await getColors(imagePathList[i]).then(colors => {
                             return colors.map(color => color.hex()).toString();
                         });
                         newVideos[i]['colors'] = colorString;
-                        console.log('saving color: ' + newVideos[i]['colors']);
                     }
                 }
 
-                console.log('finished (i think?) saving colors of images, now ready to save videos to DB');
                 return newVideos;
             })
             .then(async (newVideos) => {
-                if (newVideos[0]['colors']) {
-                    console.log('\n\nFOUND A COLOR!!!' + newVideos[0]['colors'] + '\n\n');
-                }
                 await sequelize.models.Video.bulkCreate(newVideos, { transaction })
                     .then(() => {
-                        console.log('\n\n successfully saved the videos');
-
                         return newVideos;
                     });
             });
