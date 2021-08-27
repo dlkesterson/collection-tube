88a1d5c9a8e6a9c6b33a8aa39f113e27b5d018b7
dlkesterson <dlkesterson@gmail.com>
2021-08-27 14:50:11 -0500

    db update and improved layout fade animation

--- components/wrap/index.tsx
+++ components/wrap/index.tsx
@@ -1,9 +1,16 @@
 import { motion } from 'framer-motion';
 
+const spring = {
+    type: "easeIn",
+    damping: 10,
+    stiffness: 100
+}
+
 function Wrap({ className = '', inlineStyle = {}, children }) {
     return (
         <motion.div
-            initial={{ opacity: 0 }}
+            transition={spring}
+            initial={{ opacity: 0.5 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             className={`flex flex-grow flex-col flex-nowrap justify-start items-center h-full py-2 px-8${
