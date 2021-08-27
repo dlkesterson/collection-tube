d91bf0ad985bc4f817feadc0c0eaed240f5eeb1b
dlkesterson <dlkesterson@gmail.com>
2021-08-27 15:25:44 -0500

    cleaned up the file some more

--- pages/api/get-downloads.ts
+++ pages/api/get-downloads.ts
@@ -4,22 +4,22 @@ const path = require('path');
 const fs = require('fs');
 
 async function throughDirectory(dir) {
+    const filepaths = fs.readdirSync(dir);
     let videolist = [];
     let files = [];
-    const filepaths = fs.readdirSync(dir);
-    // console.log('through dir: ' + dir);
     for (const file of filepaths) {
-    // fs.readdirSync(dir).forEach(file => {
-        // console.log(file);
         const absolute = path.join(dir, file);
-        if (fs.statSync(absolute).isDirectory()) {
+        const stats = fs.statSync(absolute);
+        if (stats.isDirectory()) {
             return throughDirectory(absolute);
         } else if (file.includes('.mp4')) {
-            console.log('\n\nfile IS A VIDEEEOOOOOO: ' + file);
             files.push(dir + '/' + file);
-            videolist.push(file);
+            videolist.push({ 
+                file, 
+                size: `${(stats.size/(1024*1024)).toFixed(2)}mb`,
+                video_id: file.slice(0,file.length-4)
+            });
         }
-    // });
     }
     return {
         files,
@@ -27,67 +27,32 @@ async function throughDirectory(dir) {
     };
 }
 
+export async function getDownloads() {
+    const { files, videolist } = await throughDirectory("./public/data/");
+    const videos = await sequelize.models.Video.findAll({
+        where: {
+            video_id: videolist.map(v => v.video_id)
+        },
+        raw: true
+    });
+
+    // for each download, append file data to the DB data
+    videos.map((video, i) => {
+        video['color'] = video['colors'].split(',')[0];
+        video['file_size'] = videolist[i]['size'];
+        video['file_name'] = videolist[i]['file'];
+    })
+
+    return { files, videos };
+}
+
 const handler: NextApiHandler = async (_, res) => {
-    let files  = [];
     try {
-        const { files, videolist } = await throughDirectory("./public/data/");
-
-        const videoIdList = videolist.map(file => {
-            return file.slice(0,file.length-4);
-        })
-
-        console.log(videoIdList);
-
-        const videoData = sequelize.models.Video.findAll({
-            where: {
-                video_id: videoIdList
-            }
-        })
-
-        // console.log(videoData);
-
-        res.status(200).send(videoData);
-
+        const data = await getDownloads();
+        res.status(200).send(data);
     } catch (e) {
         res.status(500).json({ message: e.message })
     }
 }
 
 export default handler;
-
-export async function getDownloads() {
-    // let files  = [];
-        // const channels = sequelize.models.Channel.findAll();
-        // return channels;
-
-        //joining path of directory 
-        const { files, videolist } = await throughDirectory("./public/data/");
-
-        const videoIdList = videolist.map(file => {
-            return file.slice(0,file.length-4);
-        });
-
-        console.log(videoIdList);
-
-        const videos = await sequelize.models.Video.findAll({
-            where: {
-                video_id: videoIdList
-            },
-            raw: true
-        });
-
-        return { files, videoIdList, videos };
-        // sequelize.models.Video.findAll({
-        //     where: {
-        //         video_id: videoIdList
-        //     }
-        // })
-        // .then((videos) => { 
-        //     console.log(videos);
-        //     console.log('found ' + videos.length + ' videos');
-        //     return { files, videoIdList, videos } })
-        // .catch((err) => {
-        //     console.log('There was an error querying video', JSON.stringify(err))
-        //     return err;
-        // });
-}
